f8f8922ea1c34c012add8016fd08deb5ef9b09d5
Bart Agapinan <bart@fountain.com>
2023-06-23 00:56:11 +0000

    Add annotations to models

--- test/dummy/app/models/item.rb
+++ test/dummy/app/models/item.rb
@@ -3,3 +3,20 @@ class Item < ApplicationRecord
   has_one :item_description, dependent: :destroy
   has_many :item_prices, dependent: :destroy
 end
+
+#------------------------------------------------------------------------------
+# Item
+#
+# Name       SQL Type             Null    Primary Default
+# ---------- -------------------- ------- ------- ----------
+# id         bigint               false   true
+# user_id    bigint               true    false
+# created_at timestamp without time zone false   false
+# updated_at timestamp without time zone false   false
+# name       character varying    true    false
+# col_int    integer              true    false
+# col_float  double precision     true    false
+# col_string character varying    true    false
+# klass      character varying    true    false   Item
+#
+#------------------------------------------------------------------------------
--- test/dummy/app/models/user_profile_pic.rb
+++ test/dummy/app/models/user_profile_pic.rb
@@ -1,2 +1,18 @@
 class UserProfilePic < ApplicationRecord
 end
+
+#------------------------------------------------------------------------------
+# UserProfilePic
+#
+# Name            SQL Type             Null    Primary Default
+# --------------- -------------------- ------- ------- ----------
+# id              bigint               false   true
+# user_profile_id bigint               true    false
+# image_url       character varying    true    false
+# image_width     integer              true    false
+# image_height    integer              true    false
+# created_at      timestamp without time zone false   false
+# updated_at      timestamp without time zone false   false
+# klass           character varying    true    false   UserProfilePic
+#
+#------------------------------------------------------------------------------
