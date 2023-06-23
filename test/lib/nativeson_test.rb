42303bad14d265194e2e08d5cca0d3de021ab634
Bart Agapinan <bart@sonic.net>
2023-06-23 07:29:46 +0000

    Add more tests and fixtures

--- test/lib/nativeson_test.rb
+++ test/lib/nativeson_test.rb
@@ -4,29 +4,46 @@ class NativesonTest < ActiveSupport::TestCase
   test "fetch_json_by_query_hash" do
     query_hash = {
       klass: "User",
-      columns: ["id", "name"],
+      columns: ["name"],
       associations: {
         items: {
           klass: "Item",
-          columns: ["id", "name"]
+          columns: ["name"]
+        },
+        widgets: {
+          klass: "Widget",
+          columns: ["name"]
         }
       }
     }
     expected_json = <<~JSON
-      {
-        "id": 1,
-        "name": "User 1",
-      }
+    [{"name":"Homer Simpson","items":null,"widgets":[{"name":"Green Glowy Thing"}]}, 
+     {"name":"Bart Simpson","items":[{"name":"Huffy"}],"widgets":null}]
     JSON
     actual_json = Nativeson.fetch_json_by_query_hash(query_hash)[:json]
-    assert_equal expected_json, actual_json
+
+    assert_equal expected_json.strip, actual_json.strip
   end
 
   test "fetch_json_by_rails_query" do 
+    rails_query = User.select(:name, :email)
+    expected_json = <<~JSON
+    [{"name":"Homer Simpson","email":"homer.simpson@springfield.gov"}, 
+     {"name":"Bart Simpson","email":"bart@geocities.com"}]
+    JSON
+    actual_json = Nativeson.fetch_json_by_rails_query(rails_query)[:json]
 
+    assert_equal expected_json.strip, actual_json.strip
   end
 
   test "fetch_json_by_string" do 
+    sql_string = "select name, email from users order by name"
+    expected_json = <<~JSON
+    [{"name":"Bart Simpson","email":"bart@geocities.com"}, 
+     {"name":"Homer Simpson","email":"homer.simpson@springfield.gov"}]
+    JSON
+    actual_json = Nativeson.fetch_json_by_string(sql_string)[:json]
 
+    assert_equal expected_json.strip, actual_json.strip
   end
 end
--- test/nativeson_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Nativeson::Test < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Nativeson
-  end
-end
